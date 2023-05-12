78deadeb044cba692774955c0996221d1098555a
Lars_und_so <46791248+Larsundso@users.noreply.github.com>
2023-05-12 11:37:00 +0200

    readme and npm compatibility

---
+++ dist/index.js
@@ -0,0 +1,22 @@
+import fetch from 'node-fetch';
+export const baseURL = 'https://api.waifu.pics';
+async function f(category, options) {
+ const response = await fetch(
+  `${baseURL}${options?.many ? '/many' : ''}/${options?.nsfw ? 'nsfw' : 'sfw'}/${category}`,
+  {
+   headers: { 'Content-Type': 'application/json' },
+   method: options?.many ? 'POST' : 'GET',
+   body: JSON.stringify({ exclude: options?.exclude ?? [] }),
+  },
+ );
+ if (!response.ok) {
+  throw new Error((await response.json()).message);
+ }
+ const json = await response.json();
+ if ('files' in json) return json.files;
+ return json.url;
+}
+export default f;
+//# sourceMappingURL=index.js.map
+
+console.log(await f('neko', { nsfw: true, many: true, exclude: ['https://i.waifu.pics/cjcIyzs.jpg'] }));
